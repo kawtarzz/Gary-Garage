c47ccdc524316e877d340711c7fd2b872c39a652
kawtarzz <kawtaryazzouzi@gmail.com>
2023-06-06 14:28:29 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Gary_Garage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
